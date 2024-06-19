41380814e8695229fc73b4f031e89fa820cb3fe9
Sanjib Subedi <sanjibsubedi2001@gmail.com>
2024-06-19 13:30:49 +0545

    Replaced wlogout with rofi as power menu

---
+++ .config/rofi/scripts/power_menu.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+options=" Shutdown\n Reboot\n󰍃 Logout\n Suspend\n⏼ Hibernate\n Lock"
+
+selected_option=$(echo -e $options | rofi -dmenu -p "Power Menu" -theme ~/.config/rofi/power-menu.rasi)
+
+case $selected_option in
+    *Shutdown)
+        systemctl poweroff
+        ;;
+    *Reboot)
+        systemctl reboot
+        ;;
+    *Logout)
+        hyprctl dispatch exit
+        ;;
+    *Suspend)
+        systemctl suspend
+        ;;
+    *Hibernate)
+        systemctl hibernate
+        ;;
+    *Lock)
+        hyprctl dispatch lock
+        ;;
+    *)
+        ;;
+esac
